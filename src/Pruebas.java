77929f2f04f7501ce75ac117a56ca98b131bc27d
Javier Román <javier.romloz@gmail.com>
2026-08-20 11:46:17 +0200

    Update gitignore and Main

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Pruebas {
+    public static void main(String[] args) {
+        ArrayList<String> tareas = new ArrayList<>();
+        tareas.add("hola");
+    }
 }
