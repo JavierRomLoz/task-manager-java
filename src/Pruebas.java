fbf05827f9f0c30ee269eb6a468caeeb49780cf8
Javier Román <javier.romloz@gmail.com>
2026-08-20 11:50:10 +0200

    Remove Pruebas.java from the repository and add it to .gitignore

--- src/Pruebas.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Pruebas {
-    public static void main(String[] args) {
-        ArrayList<String> tareas = new ArrayList<>();
-        tareas.add("hola");
-    }
-}
